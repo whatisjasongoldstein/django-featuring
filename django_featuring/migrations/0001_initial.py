c4d122106198e8dac3c04e259e13425858f95cb5
Jason Goldstein <jason@betheshoe.com>
2014-09-20 15:29:00 -0400

    Update to Django 1.7 migrations

--- django_featuring/migrations/0001_initial.py
+++ django_featuring/migrations/0001_initial.py
@@ -1,79 +1,41 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Dashboard'
-        db.create_table(u'django_featuring_dashboard', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50)),
-        ))
-        db.send_create_signal(u'django_featuring', ['Dashboard'])
+    dependencies = [
+        ('sites', '0001_initial'),
+        ('contenttypes', '0001_initial'),
+    ]
 
-        # Adding M2M table for field sites on 'Dashboard'
-        m2m_table_name = db.shorten_name(u'django_featuring_dashboard_sites')
-        db.create_table(m2m_table_name, (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('dashboard', models.ForeignKey(orm[u'django_featuring.dashboard'], null=False)),
-            ('site', models.ForeignKey(orm[u'sites.site'], null=False))
-        ))
-        db.create_unique(m2m_table_name, ['dashboard_id', 'site_id'])
-
-        # Adding model 'Thing'
-        db.create_table(u'django_featuring_thing', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('dashboard', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['django_featuring.Dashboard'])),
-            ('order', self.gf('django.db.models.fields.IntegerField')()),
-            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
-            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')()),
-        ))
-        db.send_create_signal(u'django_featuring', ['Thing'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Dashboard'
-        db.delete_table(u'django_featuring_dashboard')
-
-        # Removing M2M table for field sites on 'Dashboard'
-        db.delete_table(db.shorten_name(u'django_featuring_dashboard_sites'))
-
-        # Deleting model 'Thing'
-        db.delete_table(u'django_featuring_thing')
-
-
-    models = {
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        u'django_featuring.dashboard': {
-            'Meta': {'object_name': 'Dashboard'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'sites': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['sites.Site']", 'symmetrical': 'False', 'blank': 'True'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
-        },
-        u'django_featuring.thing': {
-            'Meta': {'object_name': 'Thing'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            'dashboard': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['django_featuring.Dashboard']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
-            'order': ('django.db.models.fields.IntegerField', [], {})
-        },
-        u'sites.site': {
-            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
-            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        }
-    }
-
-    complete_apps = ['django_featuring']
+    operations = [
+        migrations.CreateModel(
+            name='Dashboard',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('slug', models.SlugField(unique=True)),
+                ('sites', models.ManyToManyField(to='sites.Site', blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Thing',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('order', models.IntegerField()),
+                ('object_id', models.PositiveIntegerField()),
+                ('template', models.CharField(default=b'', help_text=b'Use a custom template for this item.', max_length=255, blank=True)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('dashboard', models.ForeignKey(related_name=b'things', to='django_featuring.Dashboard')),
+            ],
+            options={
+                'ordering': ['order'],
+            },
+            bases=(models.Model,),
+        ),
+    ]
